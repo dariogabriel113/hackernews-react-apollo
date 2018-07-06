c6d390f8f862b0976bd550ae27b0e13f9957f4ba
Dario <dariogabriel113@gmail.com>
2018-07-06 16:07:24 -0300

    Add Link.js

---
+++ src/components/Link.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Link extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    {this.props.link.description} ({this.props.link.url})
+                </div>
+            </div>
+        )
+    }
+
+    _voteForLink = async () => {
+        //
+    }
+}
+export default Link

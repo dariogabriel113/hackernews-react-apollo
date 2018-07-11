1a1fd77963488e5c4c4dbf1d55d9dd747aa8de81
Dario <dariogabriel113@gmail.com>
2018-07-11 00:00:12 -0300

    Add CreateLink

---
+++ src/components/CreateLink.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class CreateLink extends Component {
+    state = {
+        description: '',
+        url: '',
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="flex flex-column mt3">
+                    <input
+                        className="mb2"
+                        value={this.state.description}
+                        onChange={e => this.setState({ description: e.target.value })}
+                        type="text"
+                        placeholder="A description for the link"
+                    />
+                    <input
+                        className="mb2"
+                        value={this.state.url}
+                        onChange={e => this.setState({ url: e.target.value })}
+                        type="text"
+                        placeholder="The URL for the link"
+                    />
+                </div>
+                <button onClick={() => this._createLink()}>Submit</button>
+            </div>
+        )
+    }
+
+    _createLink = async () => {
+        //
+    }
+}
+
+export default CreateLink
